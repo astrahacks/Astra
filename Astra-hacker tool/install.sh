de55a31eb4ab746593009f9a6164af5290dd622b
astrahacks <91827740+astrahacks@users.noreply.github.com>
2021-10-03 22:00:39 +0530

    Add files via upload

---
+++ Astra-hacker tool/install.sh
@@ -0,0 +1,153 @@
+#!/system/xbin/bash
+#pake program bash shell
+#Ga usah Recode Ya
+#recode ga akan menjadikanmu sebagai programer
+#Tapi Kalo mempelajari Source codenya silahkan
+clear
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+echo "\033[34;1m   .............           "
+echo "\033[34;1m           ..,.:ccc,.      "                          
+echo "\033[34;1m          ......'''.lxO.   "                       
+echo "\033[34;1m .....''''..........,:ld;   "                        
+echo "\033[34;1m         .'.....:::.,,.x,       "                       
+echo "\033[34;1m      ..'''.            0Xxoc:,.  ...\\ "         
+echo "\033[34;1m  ....                ,ONkc.,.cokOdc',. \ "       
+echo "\033[34;1m .                   OMo           ':dd  . "       
+echo "\033[34;1m                   dMc              O:OO "       
+echo "\033[34;1m                  0M.                 :o. "   
+echo "\033[34;1m                    .Wd  "                         
+echo "\033[34;1m                     .XO," ==Astra ====
+echo "\033[34;1m                       ,d0Odlc.,.." By Astra Hacker
+echo "\033[34;1m                          ..',:cdOOd::,."
+echo "\033[34;1m                                    .:d;.':."
+echo "\033[34;1m                                       'd,  .'"
+echo "\033[34;1m                                         l   .."
+echo "\033[34;1m                                          .o"
+echo "\033[34;1m                                            c"
+echo "\033[34;1m                                           .'"
+echo "\033[34;1m                                            ."
+sleep 1
+echo "\033[33;1mWelcome To Astra Tools"
+sleep 1
+echo "\033[31;1mThe Ultimate Shadow Power"
+sleep 1
+echo "\\033[34;1minstalling Tools....."
+sleep 1
+echo "\033[31;1mwait...................."
+toilet -f slant -F gay "Astra Hacker" | lolcat
+echo "\033[34;1m"
+cal
+date
+sleep 1
+echo "\033[31;1mwait...................."
+sleep 1
+echo "\033[31;1m                   /\  /--\  "                
+echo "\033[31;1m           _______/  \/    1 "
+echo "\033[31;1m  /\       /____            1 "
+echo "\033[31;1m  1-------/ /__|    O  /    1 "
+echo "\033[31;1m  \--------____       /    1 "
+echo "\033[31;1m   v v v v v \ \_____/  \ 1 "
+echo "\033[31;1m   /\/\/\/\/\/\ /         \ " 
+echo "\033[31;1m  \_______________      /" 
+echo "\033[34;1m      Astra   \    / "
+echo "\033[34;1m  contact me whatsapp +919685607490 "
+echo "\033[34;1m   Powered By Astra Hacker "
+sleep 1
+echo "\033[31;1m____________________________________________ "
+echo "\033[32;1m||Tools : Astra                              ||" 
+echo "\033[32;1m||Contact me :  +919685607490                ||"
+echo "\033[32;1m||Coder : Astra Coder Crush and Silent Ghost || "
+echo "\033[32;1m||Version : 1.0                              || "
+echo "\033[31;1m||___________________________________________|| "
+echo "\033[33;1m[~Chose Tools :                              ||"
+echo "\033[31;1m||___________________________________________||"
+echo "\033[33;1m||                                           ||"
+echo "\033[31;1m|| [1].Hack Account Facebook                 ||"
+echo "\033[33;1m|| [2].Hack Account Instagram                ||"
+echo "\033[33;1m|| [3].Ddos Tools                            ||"
+echo "\033[33;1m|| [4].Install More Tools                    ||"
+echo "\033[31;1m->Press 0 To Exit                            ||"
+echo "\033[32;1m||___________________________________________||"
+read -p "root@Astra]~>" 
+
+if [ $astra = 1 ] || [ $astra = 1 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet "Hack Facebook " | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+cd Black-Dragon
+python2 black.py
+fi
+
+if [ $astra = 2 ] || [ $astra = 2 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet "Hack Instagram" | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+cd Black-Dragon
+bash ig.sh
+fi
+
+if [ $astra = 3 ] || [ $astra = 3 ]
+then
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet "Ddos Tools" | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+cd Black-Dragon
+python2 ddos.py 
+fi
+
+if [ $astra = 4 ] || [ $astra = 4 ]
+then
+clear
+echo "\033[31;1m+÷÷÷÷[]===============> "
+figlet "Installer" | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+cd Black-Dragon
+sh black.sh
+fi
+
+if [ $astra = 0 ] || [ $astra = 0 ]
+then
+echo "\033[34;1m   .............           "
+echo "\033[34;1m           ..,.:ccc,.      "                          
+echo "\033[34;1m          ......'''.lxO.   "                       
+echo "\033[34;1m .....''''..........,:ld;   "                        
+echo "\033[34;1m         .'.....:::.,,.x,       "                       
+echo "\033[34;1m      ..'''.            0Xxoc:,.  ...\\ "         
+echo "\033[34;1m  ....                ,ONkc.,.cokOdc',. \ "       
+echo "\033[34;1m .                   OMo           ':dd  . "       
+echo "\033[34;1m                   dMc              O:OO "       
+echo "\033[34;1m                  0M.                 :o. "   
+echo "\033[34;1m                    .Wd  "                         
+echo "\033[34;1m                     .XO," ==ASTRA ===
+echo "\033[34;1m                       ,d0Odlc.,.." ====By Astra Hacker===
+echo "\033[34;1m                          ..',:cdOOd::,."
+echo "\033[34;1m                                    .:d;.':."
+echo "\033[34;1m                                       'd,  .'"
+echo "\033[34;1m                                         l   .."
+echo "\033[34;1m                                          .o"
+echo "\033[34;1m                                            c"
+echo "\033[34;1m                                           .'"
+echo "\033[34;1m                                            ."
+sleep 1
+echo "\033[31;1m+÷÷÷÷[]===============> "
+toilet -f slant -F gay " Astra" | lolcat
+echo "\033[31;1m+÷÷÷÷[]===============> "
+echo "\033[33;1-Thanks Cause Use Astra Tools"
+sleep 1
+echo "\033[33;1-Please Subscribe Channel Youtube official hacker boy"
+sleep 1
+echo "\033[31;1mExit........"
+sleep 1
+exit
+fi
